f0d555510d6fe0f001023a40d153793dcbb78fd6
Joshua Chan <j.k.chan@live.com>
2018-12-26 04:45:50 -0600

    Adds in-memory database

--- src/main/java/ca/clubFinder/club/Club.java
+++ src/main/java/ca/clubFinder/club/Club.java
@@ -3,9 +3,16 @@ package ca.clubFinder.club;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Builder
 @Getter
 public class Club {
+    @Id
+    @GeneratedValue
     private final long id;
     private final String name;
 }
