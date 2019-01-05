b008b6d4f0abfb05e69929bb3a9c311c6d907f41
Bobin <bobin.chen@mail.utoronto.ca>
2019-01-04 21:01:54 -0500

    Enables CORS

--- src/main/java/ca/clubFinder/ClubFinderApplication.java
+++ src/main/java/ca/clubFinder/ClubFinderApplication.java
@@ -2,10 +2,26 @@ package ca.clubFinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ClubFinderApplication {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Adds CORS mapping for the whole application
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ClubFinderApplication.class, args);
     }
 }
+
